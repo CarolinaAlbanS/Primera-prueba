775c8b774ed7b94b76a85f90de0a35790fc6807c
carolina.alban.s <carolina-carolina.alban.sandoval@gmail.com>
2023-11-07 17:34:34 +0100

    esto sigue siendo una prueba

---
+++ Prueba1.js
@@ -0,0 +1,22 @@
+
+<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <title></title>
+	</head>
+	<body>
+    <h1>Carolina Albán Sandoval</h1>
+    <h2>Ciudad</h2>
+    <p>Nací en colombia pero llevo viviendo en Madrid muchos años.</p>
+    <h3>Formación</h3>
+    <p>Tengo titulacion como Tecnico Superior en Integración Social</p>
+    <h3>Experiencia en programación</h3>
+    <p>No tengo experiencia con la programación, lo maximo que he hecho es empezar un curso de Python en agosto.</p>
+    <ul>Aficiones:</ul>
+    <li>Diamond painting</li>
+    <li>K-dramas</li>
+    <li>Pintar</li>
+    <li>Rol</li>
+    <h3>Muchas gracias</h3>
+	</body>
+</html>
